a02a3e7f53354e45dbe47cb125b622af0308cdcd
qian-qiu-yi-meng <132723135+qian-qiu-yi-meng@users.noreply.github.com>
2023-05-24 12:41:09 +0800

    Add files via upload

---
+++ Common/LogHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class LogHelper
+    {
+        public static log4net.ILog Log = log4net.LogManager.GetLogger("MV Log");
+    }
+}
