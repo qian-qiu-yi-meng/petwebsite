83cb5a79dc693209938e8f28b35970dc503f0cc9
qian-qiu-yi-meng <132723135+qian-qiu-yi-meng@users.noreply.github.com>
2023-05-24 12:40:06 +0800

    Add files via upload

---
+++ BLL/UserInfoBLL.cs
@@ -0,0 +1,29 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class UserInfoBLL
+    {
+        public static string SelectInfoBLL(string user,string pwd,string code)
+        {
+            return DAL.UserInfoDAL.SelectUserInfo(user, pwd,code);
+        }
+        public static string SelectUserInfos(string userName, string userPwd,string code)
+        {
+            return DAL.UserInfoDAL.SelectUserInfo(userName,userPwd, code);
+        }
+        public static string GetValidateCode()
+        {
+            return DAL.UserInfoDAL.GetValidateCode();
+        }
+        public static bool GetAllUserInfo()
+        {
+            return DAL.UserInfoDAL.GetAllUserInfo();
+        }
+    }
+}
