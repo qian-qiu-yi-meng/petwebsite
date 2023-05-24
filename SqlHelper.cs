30a4a0fa23279645a674e664b4b4bbbaecd33661
qian-qiu-yi-meng <132723135+qian-qiu-yi-meng@users.noreply.github.com>
2023-05-24 15:34:26 +0800

    Add files via upload

---
+++ SqlHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Web.UI.WebControls.WebParts;
+
+namespace DAL
+{
+    public class SqlHelper
+    {
+        private static readonly string connString = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+
+        public static DataTable GetTable(string sql, CommandType type,params MySqlParameter[] pars)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                using (MySqlDataAdapter apter = new MySqlDataAdapter(sql, conn))
+                {
+                    apter.SelectCommand.CommandType = type;
+                    if (pars != null)
+                    {
+                        apter.SelectCommand.Parameters.AddRange(pars);
+                    }
+                    DataTable da = new DataTable();
+                    apter.Fill(da);
+                    return da;
+                }
+            }
+        }
+        public static int ExecuteNonquery(string sql,CommandType type,params MySqlParameter[] pars)
+        {
+            using(MySqlConnection conn =new MySqlConnection(connString))
+            {
+                using(MySqlCommand cmd =new MySqlCommand(sql, conn))
+                {
+                    cmd.CommandType = type;
+                    if (pars!=null)
+                    {
+                        cmd.Parameters.AddRange(pars);
+                    }
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static DataTable SelectAllDate(string sql)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                using (MySqlDataAdapter apter = new MySqlDataAdapter(sql, conn))
+                {
+                    DataTable da = new DataTable();
+                    apter.Fill(da);
+                    return da;
+                }
+            }
+        }
+        public static int ExecuteSql(string SQLString)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(SQLString, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        int rows = cmd.ExecuteNonQuery();
+                        return rows;
+                    }
+                    catch (MySql.Data.MySqlClient.MySqlException e)
+                    {
+                        connection.Close();
+                        throw e;
+                    }
+                }
+            }
+        }
+    }
+}
